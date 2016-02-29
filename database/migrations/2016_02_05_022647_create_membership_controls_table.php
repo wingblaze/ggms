91eceacca75513b46764c6295905f47e151e6235
Paolo Vergara <paolovergara2004@yahoo.com>
2016-02-29 18:08:37 +0800

    Added comments to membership_controls_table.

--- database/migrations/2016_02_05_022647_create_membership_controls_table.php
+++ database/migrations/2016_02_05_022647_create_membership_controls_table.php
@@ -15,9 +15,9 @@ class CreateMembershipControlsTable extends Migration
         Schema::create('membership_controls', function (Blueprint $table) {
             $table->increments('id');
 
-            $table->integer('posted_by_account_id')->unsigned()->nullable();
-            $table->integer('current_account_id')->unsigned()->nullable();
-            $table->integer('membership_slot_id')->unsigned()->nullable();
+            $table->integer('posted_by_account_id')->unsigned()->nullable()->comment = "The ID of the account who posted this club share listing.";
+            $table->integer('current_account_id')->unsigned()->nullable()->comment = "The ID of the account who is currently holding this listing. New club share listings set this to NULL, which means this listing is not yet sold.";
+            $table->integer('membership_slot_id')->unsigned()->nullable()->comment = "This is the membership slot being sold or transfered.";
             $table->unique( array('current_account_id','membership_slot_id') );
 
             $table->timestamps();
