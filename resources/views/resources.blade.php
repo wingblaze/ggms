d515c048e615a2346b94359d11a44e2d455106bb
Paolo Vergara <pal.vergara@gmail.com>
2016-04-10 23:50:19 +0800

    Updated facilities view. Removed date created for non sys-admin users. Removed options column.

--- resources/views/resources.blade.php
+++ resources/views/resources.blade.php
@@ -13,8 +13,8 @@
 		<table class="table table-striped">
 			<tr>
 				<th class="col-md-8">Name of Facility</th>
-				<th class="col-md-2">Date created</th>
 				@if ($user->hasRole('system_administrator'))
+				<th class="col-md-2">Date created</th>
 				<th class="col-md-4">Options</th>
 				@endif
 			</tr>
@@ -25,8 +25,8 @@
 						{{ $resource->name }}
 					</a>
 				</td>
-				<td class="col-md-2">{{ $resource->created_at or 'Unspecified' }}</td>
 				@if ($user->hasRole('system_administrator'))
+				<td class="col-md-2">{{ $resource->created_at or 'Unspecified' }}</td>
 				<td class="col-md-4">
 					
 					<a class="btn btn-sm btn-default" href="{{action('ResourceController@edit', ['id' => $resource->id])}}"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>&nbsp Edit</a>
@@ -50,10 +50,9 @@
 		<table class="table table-striped">
 			<tr>
 				<th class="col-md-3">Name of Facility and Renter</th>
+				<th class="col-md-1">Status</th>
 				<th class="col-md-1">Rental start time</th>
 				<th class="col-md-1">Rental end time</th>
-				<th class="col-md-1">Status</th>
-				<th class="col-md-4">Options</th>
 			</tr>
 			
 			@foreach($listings as $listing)
@@ -67,17 +66,10 @@
 					 	{{ $listing->user->display_name }}
 					</a>
 				</td>
-				<td class="col-md-3">{{ Carbon\Carbon::parse($listing->start_time)->format('h:i:s A') }}</td>
-				<td class="col-md-3">{{ Carbon\Carbon::parse($listing->end_time)->diffForHumans() }}<BR />
-					{{ Carbon\Carbon::parse($listing->end_time)->format('F jS Y, h:i:s A') }}
-				</td>
 				<td class="col-md-1">{{ $listing->status }}</td>
-				<td class="col-md-4">
-					@if ($user->hasRole('employee'))
-					<a class="btn btn-sm btn-default" href="{{action('ResourceController@edit', ['id' => $listing->id])}}"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>&nbsp Edit</a>
-					@else
-					  None
-					@endif
+				<td class="col-md-1">{{ Carbon\Carbon::parse($listing->start_time)->format('h:i A') }}</td>
+				<td class="col-md-1">
+					{{ Carbon\Carbon::parse($listing->end_time)->format('h:i A') }}
 				</td>
 			</tr>
 			@endforeach
